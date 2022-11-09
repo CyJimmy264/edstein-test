816d45c001ebdd20f82ddcc8ca5859c01389f7b3
mveynberg <mv@cj264.ru>
2022-11-09 20:25:46 +0500

    Use Rswag

---
+++ spec/requests/api/health_spec.rb
@@ -0,0 +1,18 @@
+require 'swagger_helper'
+
+describe 'Health API' do
+  path '/health' do
+    get 'Retrieves a health' do
+      tags 'Health'
+      produces 'application/json'
+
+      response '200', 'health ok' do
+        schema type: :object,
+               properties: {
+                 status: { type: :string },
+               }
+        run_test!
+      end
+    end
+  end
+end
